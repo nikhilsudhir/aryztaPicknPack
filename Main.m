9851beaa8c181e4bf3d3f59ece710f8300dd46f8
Abael Joseph <abael.joseph@gmail.com>
2024-11-04 03:21:25 +1100

    Update Main.m
    Added the Testing collision and Testing Light Curtain functions to the main function. Used by uncommenting the code.

--- Main.m
+++ Main.m
@@ -22,5 +22,13 @@ movePancakes(UR3e2, 4, -13, -10, -4);
 boxCollection(UR30, 0, 1, 1.4, 2.8, -1.5, 0.5);
 boxCollection(UR30, -1.4, 1, 1.4, 1.85, -0.3, 0.7);
 
+% %% Testing collision detection of UR30
+% CollisionDetectionUR30(UR30, -1.4, 1, 1, 2.3, -0.7, 0.45, 0.8, 1.5, 1);
+% pause(11.5);
+% CollisionDetectionUR30(UR30, 1.4, 1.4, 0, 2.3, -0.7, 0.8, 0.45, 3, -1);
 
 
+% %% Testing Light Curtain Object detection
+% stopSignalByUser([1.1, -3, 0],-1.7, 90, "UR30");
+% 
+% stopSignalByUser([-6.9, 3.8, 0],2.1, -90, "UR3e");
